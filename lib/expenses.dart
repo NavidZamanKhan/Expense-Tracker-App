e383672e0f622643e4311e3cacf4570e7664acd8
Navid444 <navid166418@gmail.com>
2025-03-02 00:24:03 +0600

    added main and expenses

---
+++ lib/expenses.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(children: [Text("chart"), Text("list of transactions")]),
+    );
+  }
+}
