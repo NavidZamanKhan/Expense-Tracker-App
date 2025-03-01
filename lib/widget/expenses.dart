c585fa46c8a884bedd70156308277efe67d4f6c4
Navid444 <navid166418@gmail.com>
2025-03-02 02:58:54 +0600

    added expenses widgets

---
+++ lib/widget/expenses.dart
@@ -0,0 +1,53 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Groceries",
+      amount: 45.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "New Shoes",
+      amount: 89.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Flight to Paris",
+      amount: 299.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("chart"),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
